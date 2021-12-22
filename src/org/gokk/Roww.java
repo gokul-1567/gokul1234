b37c3c1bb1f7e934a8042f1164c71d300c405c60
gokul-1567 <gokulre98@gmail.com>
2021-12-22 16:25:38 +0530

    B updated

--- src/org/gokk/Roww.java
+++ src/org/gokk/Roww.java
@@ -1,10 +1,15 @@
 package org.gokk;
 
 public class Roww {
-private void met1() {
-System.out.println("new method");
-}
-private void notAdd() {
-System.out.println("not added");
-}
+	private void met1() {
+		System.out.println("new method");
+	}
+
+	private void notAdd() {
+		System.out.println("not added");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
